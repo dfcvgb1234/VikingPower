d1aacdc538e3aa3a2243655770765a5cdfac4cf4
IceBerg <www.favs2@gmail.com>
2017-04-25 09:46:10 +0200

    Update

---
+++ VikingQuest/Assets/Follow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+    public GameObject follow;
+    public float offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.position = follow.transform.position + new Vector3(0, 0, offset);
+	}
+}
---
+++ VikingQuest/Assets/WalkingScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingScript : MonoBehaviour {
+    public Sprite walkingSprite;
+    public Sprite upSprite;
+    public Sprite downSprite;
+    public int speed = 100;
+    SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+        sr = this.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveWASD();
+	}
+
+    void MoveWASD()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(new Vector3(-2, 0) * Time.deltaTime * speed);
+            sr.flipX = true;
+            sr.sprite = walkingSprite;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(new Vector3(2, 0) * Time.deltaTime * speed);
+            sr.flipX = false;
+            sr.sprite = walkingSprite;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(new Vector3(0, 2) * Time.deltaTime * speed);
+            sr.sprite = upSprite;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(new Vector3(0, -2) * Time.deltaTime * speed);
+            sr.sprite = downSprite;
+        }
+    }
+}
