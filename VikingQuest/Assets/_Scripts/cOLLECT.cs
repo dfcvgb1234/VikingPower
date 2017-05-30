c3d6c0bebc1d761af8a7afe664d44d2755dd24fc
TheGejr <maltegejr.korup@gmail.com>
2017-05-30 10:14:11 +0200

    Lorte projekt

---
+++ VikingQuest/Assets/_Scripts/cOLLECT.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cOLLECT : MonoBehaviour {
+
+	public Text score;
+	static public int scroew = 0;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "rav") {
+			scroew++;
+			score.text = "Gigant Rav : " + scroew;
+
+            other.gameObject.transform.position = new Vector3(Random.Range(64.77667f, 3161f), Random.Range(-1501f, -52f));
+
+			//other.GetComponent<SpriteRenderer> ().sprite = null;
+			//Destroy (other);
+		}
+	}
+}
---
+++ VikingQuest/Assets/shit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class shit : MonoBehaviour
+{
+
+    public Button newGameButton;
+    public Text textshit;
+
+    void Awake()
+    {
+        textshit.text = "Du samlede " + cOLLECT.scroew + " stykker rav!";
+    }
+
+    void OnEnable()
+    {
+        newGameButton.onClick.AddListener(delegate
+        {
+            OnNewGamePress();
+        });
+    }
+
+    void OnNewGamePress()
+    {
+        SceneManager.LoadScene("World");
+    }
+}
